2bb3713208a34dab39640154c47db974715d0666
XuanCheng <1454421048@qq.com>
2022-09-18 14:33:24 +0800

    更新了算法

--- level1/p02_isPrime/isPrime.cpp
+++ level1/p02_isPrime/isPrime.cpp
@@ -1,17 +1,19 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-	int n,k;
+	int n,k,a,b;
 	int i = 2;
 	scanf("%d",&n);
-	for(;i <= n;i++)
-	{
-		if(n%i == 0)
+	a = sqrt((double)n);
+	for(i = 2;i <= a;i++)
 		{
-			break;
+			if(n%i == 0)
+			{
+				break;
+			}
 		}
-	}
-	if(i == n)
+	if(i == a+1)
 	{
 		printf("Yes");
 	}else printf("No");
