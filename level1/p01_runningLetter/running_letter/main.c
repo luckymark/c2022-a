4f50360b146971d21f9f221c9a4d593e58e136a1
luckymark <luckyzhigang@gmail.com>
2022-09-19 21:16:55 +0800

    running lettert

---
+++ level1/p01_runningLetter/running_letter/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libc.h>
+
+int main() {
+    while(1){
+        system("clear");
+        printf("Hello, World!\n");
+        sleep(1);
+        system("clear");
+        printf("Hello,           World!\n");
+        sleep(1);
+    }
+
+    return 0;
+}
