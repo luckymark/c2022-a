430da8aef1d0db4c6f7247d36dbf5cf81bac0472
LeoAltorea <1642554007@qq.com>
2022-09-20 15:16:01 +0800

    完成了running letter

---
+++ level1/p01_runningLetter/running letter.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 51
+int main() {
+FLAG01:
+	int step = 0, path = 0;
+	while (step < SIZE) {
+		if (step == SIZE-1) 
+			goto FLAG;
+		system("cls");
+		path = 1;
+		while (path <= step) {
+			printf(" ");
+			path++;
+		}
+		printf("гд");
+		step++;
+	}
+
+FLAG:
+	while (step > 0) {
+		if (step == 1) 
+			goto FLAG01;
+		system("cls");
+		path = 1;
+		while (step >= path) {
+			printf(" ");
+			path++;
+		}
+		step--;
+		printf("гд");
+	}
+	return 0;
+}
