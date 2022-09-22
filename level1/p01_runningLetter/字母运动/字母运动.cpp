008b46e2c3c33e15c37d7c516b67ffc6e2dcf414
Feiwuguanfang <110074865+Feiwuguanfang@users.noreply.github.com>
2022-09-22 20:12:30 +0800

    Add files via upload

---
+++ level1/p01_runningLetter/字母运动/字母运动.cpp
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+int main()
+{
+	int stage = 1;
+	int pace = 1;
+	while (1)
+	{
+		for (int i = 0; i <= stage; i++)
+			printf(" "); 
+
+		if (stage == 80 || stage == 0)
+			pace = -pace;
+			
+		stage += pace;
+		printf("hello world");
+		Sleep(100);
+		system("cls");
+	}
+	 return 0;
+	
+}
+
+
+
