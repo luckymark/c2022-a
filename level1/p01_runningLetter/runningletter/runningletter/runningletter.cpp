8451764fb26dfcc505bf70b1c509118b769a51c5
dengdaiGodot <113399058+dengdaiGodot@users.noreply.github.com>
2022-09-26 22:15:06 +0800

    running letter

---
+++ level1/p01_runningLetter/runningletter/runningletter/runningletter.cpp
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+int main()                                      //第一个作业
+{
+	const int left = 0;
+	const int right = 120;
+	int a = 1, b = 0;
+	while (1)
+	{
+		b += a;
+		if (b == left || b == right)
+		{
+			a = -a;
+		}
+		for (int c = 0; c < b; c++)
+		{
+			putchar(' ');
+		}
+		puts("$$$$");
+		system("cls");
+	}
+	return 0;
+}
