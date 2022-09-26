d032b7e5fd8833ff4f0c4479095ee3edb1d83af8
XuanCheng <1454421048@qq.com>
2022-09-26 21:25:16 +0800

    将判断逻辑提取到独立的函数中

--- level1/p02_isPrime/isPrime.cpp
+++ level1/p02_isPrime/isPrime.cpp
@@ -1,10 +1,20 @@
 #include<stdio.h>
 #include<math.h>
+
+void prime(int n);
+ 
 int main()
 {
 	int n,k,a,b;
 	int i = 2;
 	scanf("%d",&n);
+	prime(n);
+	return 0;
+}
+
+void prime(int n)
+{
+	int a,i;
 	a = sqrt((double)n);
 	for(i = 2;i <= a;i++)
 		{
@@ -17,5 +27,4 @@ int main()
 	{
 		printf("Yes");
 	}else printf("No");
-	return 0;
- } 
+}
