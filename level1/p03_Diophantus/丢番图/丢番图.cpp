b2b13b67608109422eec34669dbe82be2fb0bf7b
Feiwuguanfang <3475707338@qq.com>
2022-09-28 22:37:59 +0800

    p05_allPrimes

---
+++ level1/p03_Diophantus/丢番图/丢番图.cpp
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+int main()
+{
+	float span_dad,span_son;
+	float happy = 5, sorrow = 4;
+	span_dad = (1/(1 - (float)1 / 6 - (float)1 / 12 - (float)1 / 7 - (float)1 / 2))*(happy + sorrow);
+	//(1/6+1/12+1/7)span_dad+5+span_son+4==span_dad;
+	printf("%f", span_dad);
+	return 0;
+
+}
