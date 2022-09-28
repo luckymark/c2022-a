9f8ea6b4c4de794e57c61126430a32c40e12fd1f
Kuangyuchui <2594864345@qq.com>
2022-09-28 09:26:46 +0800

    isPrime, but the algorithm is not so good

---
+++ level1/p02_isPrime/isPrime.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int prime = 0, isPrime = 0;
+	scanf("%d",&prime);
+	if(prime==1){
+		isPrime = 0;
+	}else if(prime==2||prime==3){
+		isPrime = 1;
+	}
+	for(int i =2;i<=prime/3,i++){
+		if(prime%i==0){
+		}
+	}
+}
