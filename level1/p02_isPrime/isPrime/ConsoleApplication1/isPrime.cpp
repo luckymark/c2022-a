60a6b0d86efdf88714f71a5c65d37bdf604cd604
ZhizhongKG <209490471@qq.com>
2022-10-03 00:27:43 +0800

    已完成水仙数

--- level1/p02_isPrime/isPrime/ConsoleApplication1/isPrime.cpp
+++ level1/p02_isPrime/isPrime/ConsoleApplication1/isPrime.cpp
@@ -2,22 +2,12 @@
 #include <math.h>
 void main()
 {
-    int m;  // 输入的整数 
-    int i;  // 循环次数
-    int k;  // m 的平方根 
-
-    printf("输入一个整数：");
-    scanf_s("%d", &m);
-
-    // sqrt()的参数为 double 类型，强制转换m的类型 
-    k = (int)sqrt((double)m);
-    for (i = 2; i <= k; i++)
-        if (m % i == 0)
-            break;
-
-    // 如果完成所有循环，那么m为素数
-    if (i > k)
-        printf("%d是素数。\n", m);
-    else
-        printf("%d不是素数。\n", m);
+	int a, b, sum.c d;
+		double e;
+		scanf_s("%d %d", &a, &b);
+		sum = a + b;
+		c = a - b;
+		d = a * b;
+		e = a / b;
+		printf("sum=%d,c=%d,d=%d,e=%.2f\n", sum, c, d, e);
 }
