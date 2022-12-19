fe55f793684b313094bf1db5d021927ab2f53368
1yc0 <1594145639@qq.com>
2022-12-19 12:28:50 +0800

    交作业

---
+++ level1/p05_allPrimes/allPrimes.cpp
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+#include<time.h>
+int main() {
+	int i;
+	for (i = 2;i <= 1000;i++)
+	{
+		int isPrime = 1;
+		int k;
+		for (k = 2;k < i - 1;k++) {
+			if (i % k == 0) {
+				isPrime = 0;
+				break;
+			}
+		}
+		if (isPrime) {
+			printf("%d ", i);
+		}
+	}
+	printf("\nTime used = %.2f\n", (double)clock() / CLOCKS_PER_SEC);
+	return 0;
+}
+
