006eaf55a95518d478439732339d3273596b4028
Akollector <401325518@qq.com>
2023-01-02 15:40:10 +0800

    Diophantus,narcissus and allPrimes finished for the 1st time

---
+++ level1/p03_Diophantus/Diophantus.cpp
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+
+int life;
+
+
+int main (void)
+{
+	//life=life/6+life/12+life/7+5+life/2+4
+	life = (5 + 4) * 28 / 3;
+	printf("The age of Diophantus is %d",life);
+	return 0;
+}
---
+++ level1/p04_ narcissus/narcissus.cpp
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+#include<math.h>
+using namespace std;
+
+int num = 0, hundred = 1, tens = 0,one=0;
+
+int main(void)
+{
+	printf("All the 3-digit narcissus numbers are:\n");
+	for (int num = 100; num < 1000; num++)
+	{
+		one = num % 10;
+		tens = num /10 %10;
+		hundred = num / 100;
+		if ((hundred * hundred * hundred + tens * tens * tens + one * one * one) == num)
+		{
+			printf("%d\n", num);
+		}
+	}
+	return 0;
+}
+
