e270d361f71651497ae5355d5d50192674947e88
XuanCheng <1454421048@qq.com>
2023-02-18 21:23:02 +0800

    将判断提取到单独的函数

--- level1/p06_Goldbach/p06_Goldbach.cpp
+++ level1/p06_Goldbach/p06_Goldbach.cpp
@@ -2,50 +2,54 @@
 #include<math.h>
 
 int prime(int n);
+int Goldbach(int n);
 
 int main()
 {
-	int n,p = 2,q,a;
-	scanf("%d",&n);
-	if(n >= 4 && n <=2000 && n%2 == 0)
-	{
-		for(;p <= n;p++)
-		{
-			if(prime(p) == 0)
-			{
-				a = 0;
-			}else a = 1;
-			if(a)
-			{
-				q = n - p;
-				if(prime(q) == 0)
-				{
-					a = 0;
-				}
-			}
-			if(a)
-			{
-				printf("%d %d",p,q);
-				break;
-			}
-		}
-	}else printf("error");
-	return 0;
+    int n;
+    scanf("%d",&n);
+    Goldbach(n);
+    return 0;
 }
 
 int prime(int n)//use in if() version
 {
-	int a,i;
-	a = sqrt((double)n);
-	for(i = 2;i <= a;i++)
-		{
-			if(n%i == 0)
-			{
-				break;
-			}
-		}
-	if(i == a+1)
-	{
-		return n;
-	}else return 0;
+    int a,i;
+    a = sqrt((double)n);
+    for(i = 2;i <= a;i++)
+    {
+        if(n%i == 0)
+        {
+            break;
+        }
+    }
+    if(i == a+1)
+    {
+        return n;
+    }else return 0;
+}
+
+int Goldbach(int n){
+    int p = 2,q,a;
+    if(n >= 4 && n <=2000 && n%2 == 0){
+        for(;p <= n;p++){
+            if(prime(p) == 0)
+            {
+                a = 0;
+            }else a = 1;
+            if(a)
+            {
+                q = n - p;
+                if(prime(q) == 0)
+                {
+                    a = 0;
+                }
+            }
+            if(a)
+            {
+                printf("%d %d",p,q);
+                break;
+            }
+        }
+    }else printf("error");
 }
